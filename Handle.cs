d5cb9100af29f8fa975d21e288593c71908a34e1
nahtibitz <35587209+nahtibitz@users.noreply.github.com>
2018-02-23 18:10:25 -0500

    Added Handle class for name generator

---
+++ Handle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Handle
+    {
+        //Instance variables
+        string[] inputNames = new string[10];
+        string[] arrayOf3 = new string[20]; //Stores the names after they are trimmed and stripped
+
+        //Constructor
+        public Handle()
+        {
+
+        }
+
+        //Print the main line for the menu generator, and 
+        //create an array based off the names provided.
+        //This should be two methods, tbh
+
+        public string[] HandleOptions()
+        {
+            Console.WriteLine("Input up to 10 names that you would like to generate a name from.");
+
+            int looper = 0;
+            do
+            {
+                for (int x = 0; x < inputNames.Length; x++)
+                {
+                    inputNames[x] = Console.ReadLine();
+                    if (String.IsNullOrEmpty(inputNames[x]))
+                    {
+                        looper++;
+
+                        x = inputNames.Length;
+                    }
+                }
+            }
+            while (looper == 0);
+            return inputNames;
+
+        }
+
+        /// <summary>
+        /// Trim the whitespace down and split the strings into groups of 3 characters per name
+        /// </summary>
+        public void TrimString(string[] ArrayToTrim)
+        {
+            //Trim any whitespace
+            for (int x = 0; x < ArrayToTrim.Length; x++)
+            {
+                if (inputNames[x] == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    inputNames[x] = ArrayToTrim[x].Trim();
+                }
+            }
+        }
+
+
+        public void SplitString(string[] ArrayToSplit)
+        {
+            int looper = 0;
+            while (looper == 0)
+            {
+                for (int x = 0; x < inputNames.Length; x++)
+                {
+
+                }
+            }
+
+
+        }
+        /*TEST METHOD FOR TRIM
+          public void PrintTrim()
+        {
+            for(int x = 0; x < inputNames.Length;x++)
+                Console.WriteLine(inputNames[x]);
+        } */
+
+
+        public string[] PrintNames()
+        {
+            string[] names = new string[10];  //wtf
+
+            return names;
+        }
+
+        public void Continue()
+        {
+            Console.WriteLine("Would you like to continue generating handles?");
+
+            Console.ReadKey();
+        }
+    }
+}
--- RandomGenerator.cs
+++ RandomGenerator.cs
@@ -20,9 +20,11 @@ namespace Learning
              * Allow option for random name generator based on input?
              * ROLL FOR CRITS?!
              */
+            Handle Handle = new Handle();
             RNG RNG = new RNG();
             int menuSelection = 0;
-                                
+               
+            //Loop menu selection until they input a valid option
             int looper = 1;
             while (looper == 1)
             {
@@ -31,6 +33,7 @@ namespace Learning
                 Console.WriteLine(" 1. Dice Roller \n 2. RNG \n 3. Battle Simulator \n 4. Handle Generator \n");
                 int menuSelection1 = Int32.Parse(Console.ReadLine());
                 
+                //If menuSelection == one of the menu options, close the menu loop.
                 if (menuSelection1 == 1 || menuSelection1 == 2 || menuSelection1 == 3 || menuSelection1 == 4)
                     {
                     menuSelection = menuSelection1;
@@ -39,9 +42,21 @@ namespace Learning
                     else
                     {
                     Console.WriteLine("That is not a valid option");
+                    Console.WriteLine("Would you like to exit? y/n");
+                    string exit = Console.ReadLine();
+                    if (exit == "y")
+                        Environment.Exit(0);
+                    else
+                        continue;
+                                         
                     }
             }
            
+            //Switch that runs the "sub" application. Runs a continue from that object class
+            //at the end to continue using that sub app. 
+            //IF MENU WAS AN OBJECT IT WOULD RETURN TO MENU.
+            //TODO: Make Menu an object
+            
             switch (menuSelection)
             {
                 case 1:
@@ -55,13 +70,16 @@ namespace Learning
                     Console.WriteLine("Let's generate some rando's! \n");
                     RNG.HowMany();
                     RNG.Continue();
-
                     break;
                 case 3:
                     Console.WriteLine("Ready for battle, are we?");
                     break;
                 case 4:
                     Console.WriteLine("Let's get you a new handle.");
+                    Handle.TrimString(Handle.HandleOptions());
+
+                //  Handle.PrintTrim(); TEST METHOD FOR TRIMING THE ARRAY ENTRIES
+                    
                     break;
             }
 
